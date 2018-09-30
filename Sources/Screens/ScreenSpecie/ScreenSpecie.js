989125d4270286bdec72988661b08d59ca9a81a5
Gwen <gwenael.leroutier@gmail.com>
2018-09-30 18:57:07 +0200

    Correction d'un potentiel bug lié à la gallerie (récupération d'une valeure null dans firebase)

--- Sources/Screens/ScreenSpecie/ScreenSpecie.js
+++ Sources/Screens/ScreenSpecie/ScreenSpecie.js
@@ -33,6 +33,7 @@ class ScreenSpecie extends React.Component {
 
             specieDescription: '',
             speciePhotos: {},
+            specieAnimals: {},
 
             specieName: '',
             specieLatinName: '',
@@ -54,8 +55,8 @@ class ScreenSpecie extends React.Component {
                 specieProfilePicture: remoteData.specieProfilePicture,
                 specieLatinName: remoteData.specieLatinName,
                 specieDescription: remoteData.specieDescription,
-                speciePhotos: remoteData.speciePhotos,
-                specieAnimals: remoteData.specieAnimals,
+                speciePhotos: remoteData.speciePhotos|| {},
+                specieAnimals: remoteData.specieAnimals || {}
             });
         });
     }
