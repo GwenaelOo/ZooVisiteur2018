22edee621b5a03ffe4c47579d2f9ff2d77c900b1
Gwen <gwenael.leroutier@gmail.com>
2018-10-05 15:54:06 +0200

    Description ajout des feuilles de style dynamique

--- Sources/Screens/ScreenGwenPlayground/Description.js
+++ Sources/Screens/ScreenGwenPlayground/Description.js
@@ -1,84 +1,67 @@
 import React from 'react';
 import { View, Image, Dimensions, StyleSheet, Text } from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { iOSUIKit, material } from 'react-native-typography'
 import { LinearGradient } from 'expo';
+import returnOrientation from '../../Components/Scripts/returnOrientation'
 
 class Description extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-    
+
         };
     }
-
     render() {
+
+        const styles = ((returnOrientation() === 'portrait')?stylesPortrait : stylesPaysage)
         return (
-            <View style={[styles.container, {top: -Math.abs(hp('15%')) }]}>
-                <View name="title" style={{ marginTop: hp('12%'), marginLeft: hp('10%') }}>
+            <View style={[styles.container]}>
+                <View name="title" style={styles.title}>
                     <Text style={material.title}>{this.props.title}</Text>
                 </View>
-                <View name="Description" style={{ marginTop: hp('2%'), marginBottom: hp('5%'),  marginHorizontal: wp('10%') }}>
+                <View name="Description" style={styles.text}>
                     <Text style={[material.body1, { textAlign: 'justify' }]}>{this.props.text}</Text>
                 </View>
             </View>
         )
+
     }
 }
 
 export default Description
 
-const styles = StyleSheet.create({
-    overlay: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'black',
-        opacity: 0.55
-    },
-    itemData: {
-        backgroundColor: '#016e8d',
-        position: 'relative',
-        top: -30,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 15 },
-        shadowOpacity: 0.8,
-        shadowRadius: 5,
-        elevation: 8,
-    },
+
+const stylesPortrait = StyleSheet.create({
     container: {
         backgroundColor: 'white',
         position: 'relative',
-        top: 0,
+        top: -Math.abs(hp('15%'))
     },
-    hours: {
-        flex: 5,
-        justifyContent: 'space-around',
-        flexDirection: 'column',
-        alignItems: 'center',
+    title: {
+        marginTop: hp('12%'),
+        marginLeft: wp('10%')
     },
-    buttonRemind: {
-        flex: 3,
+    text: {
+        marginTop: hp('2%'),
+        marginBottom: hp('5%'),
+        marginHorizontal: wp('10%')
+    }
+});
+
+const stylesPaysage = StyleSheet.create({
+    container: {
+        backgroundColor: 'red',
+        position: 'relative',
+        top: -Math.abs(hp('15%'))
     },
-    vignette: {
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'space-around',
-        flexDirection: 'row',
-        alignItems: 'center',
+    title: {
+        marginTop: hp('12%'),
+        marginLeft: wp('10%')
     },
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
+    text: {
+        marginTop: hp('2%'),
+        marginBottom: hp('5%'),
+        marginHorizontal: wp('10%')
+    }
 });
